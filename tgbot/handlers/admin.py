6f4f6b671a552ec288536455945d8190cf57a1f5
AuthFailed <authfailed@gmail.com>
2024-06-28 00:47:12 +0500

    добавлено админ меню

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -1,3 +1,5 @@
+from tgbot.keyboards.inline import admin_main_menu
+
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
@@ -10,4 +12,7 @@ admin_router.message.filter(AdminFilter())
 
 @admin_router.message(CommandStart())
 async def admin_start(message: Message):
-    await message.reply("Приветствую, админ!")
+    await message.reply(
+        f"Привет, @{message.from_user.username}!\nИспользуй меню для управления ботом",
+        reply_markup=admin_main_menu(),
+    )
